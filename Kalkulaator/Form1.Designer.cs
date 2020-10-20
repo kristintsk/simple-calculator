59dc87cff65bc33eeb05ff75f8c0b97bcd57b96d
Kristin <Kristin@KRISTIN-PC>
2020-10-20 12:08:58 +0300

    first commit

---
+++ Kalkulaator/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace Kalkulaator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.KT_lbl1 = new System.Windows.Forms.Label();
+            this.KT_lbl2 = new System.Windows.Forms.Label();
+            this.KT_txtArv1 = new System.Windows.Forms.TextBox();
+            this.KT_txtArv2 = new System.Windows.Forms.TextBox();
+            this.KT_rbLiida = new System.Windows.Forms.RadioButton();
+            this.KT_rbLahuta = new System.Windows.Forms.RadioButton();
+            this.KT_rbKorruta = new System.Windows.Forms.RadioButton();
+            this.KT_rbJaga = new System.Windows.Forms.RadioButton();
+            this.KT_btn1 = new System.Windows.Forms.Button();
+            this.KT_lbl3 = new System.Windows.Forms.Label();
+            this.KT_txtVastus = new System.Windows.Forms.TextBox();
+            this.KT_btn2 = new System.Windows.Forms.Button();
+            this.KT_btnYmarda = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // KT_lbl1
+            // 
+            this.KT_lbl1.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.KT_lbl1.Location = new System.Drawing.Point(159, 71);
+            this.KT_lbl1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.KT_lbl1.Name = "KT_lbl1";
+            this.KT_lbl1.Size = new System.Drawing.Size(122, 32);
+            this.KT_lbl1.TabIndex = 0;
+            this.KT_lbl1.Text = "Esimene arv";
+            this.KT_lbl1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // KT_lbl2
+            // 
+            this.KT_lbl2.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.KT_lbl2.Location = new System.Drawing.Point(159, 155);
+            this.KT_lbl2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.KT_lbl2.Name = "KT_lbl2";
+            this.KT_lbl2.Size = new System.Drawing.Size(122, 32);
+            this.KT_lbl2.TabIndex = 1;
+            this.KT_lbl2.Text = "Teine arv";
+            this.KT_lbl2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // KT_txtArv1
+            // 
+            this.KT_txtArv1.Location = new System.Drawing.Point(302, 68);
+            this.KT_txtArv1.Name = "KT_txtArv1";
+            this.KT_txtArv1.Size = new System.Drawing.Size(100, 26);
+            this.KT_txtArv1.TabIndex = 2;
+            this.KT_txtArv1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // KT_txtArv2
+            // 
+            this.KT_txtArv2.Location = new System.Drawing.Point(302, 155);
+            this.KT_txtArv2.Name = "KT_txtArv2";
+            this.KT_txtArv2.Size = new System.Drawing.Size(100, 26);
+            this.KT_txtArv2.TabIndex = 3;
+            this.KT_txtArv2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // KT_rbLiida
+            // 
+            this.KT_rbLiida.AutoSize = true;
+            this.KT_rbLiida.Location = new System.Drawing.Point(489, 65);
+            this.KT_rbLiida.Name = "KT_rbLiida";
+            this.KT_rbLiida.Size = new System.Drawing.Size(76, 22);
+            this.KT_rbLiida.TabIndex = 4;
+            this.KT_rbLiida.TabStop = true;
+            this.KT_rbLiida.Text = "Liida";
+            this.KT_rbLiida.UseVisualStyleBackColor = true;
+            // 
+            // KT_rbLahuta
+            // 
+            this.KT_rbLahuta.AutoSize = true;
+            this.KT_rbLahuta.Location = new System.Drawing.Point(489, 102);
+            this.KT_rbLahuta.Name = "KT_rbLahuta";
+            this.KT_rbLahuta.Size = new System.Drawing.Size(86, 22);
+            this.KT_rbLahuta.TabIndex = 5;
+            this.KT_rbLahuta.TabStop = true;
+            this.KT_rbLahuta.Text = "Lahuta";
+            this.KT_rbLahuta.UseVisualStyleBackColor = true;
+            // 
+            // KT_rbKorruta
+            // 
+            this.KT_rbKorruta.AutoSize = true;
+            this.KT_rbKorruta.Location = new System.Drawing.Point(489, 140);
+            this.KT_rbKorruta.Name = "KT_rbKorruta";
+            this.KT_rbKorruta.Size = new System.Drawing.Size(96, 22);
+            this.KT_rbKorruta.TabIndex = 6;
+            this.KT_rbKorruta.TabStop = true;
+            this.KT_rbKorruta.Text = "Korruta";
+            this.KT_rbKorruta.UseVisualStyleBackColor = true;
+            // 
+            // KT_rbJaga
+            // 
+            this.KT_rbJaga.AutoSize = true;
+            this.KT_rbJaga.Location = new System.Drawing.Point(489, 178);
+            this.KT_rbJaga.Name = "KT_rbJaga";
+            this.KT_rbJaga.Size = new System.Drawing.Size(66, 22);
+            this.KT_rbJaga.TabIndex = 7;
+            this.KT_rbJaga.TabStop = true;
+            this.KT_rbJaga.Text = "Jaga";
+            this.KT_rbJaga.UseVisualStyleBackColor = true;
+            // 
+            // KT_btn1
+            // 
+            this.KT_btn1.BackColor = System.Drawing.Color.Lavender;
+            this.KT_btn1.Location = new System.Drawing.Point(333, 247);
+            this.KT_btn1.Name = "KT_btn1";
+            this.KT_btn1.Size = new System.Drawing.Size(150, 37);
+            this.KT_btn1.TabIndex = 8;
+            this.KT_btn1.Text = "Arvuta";
+            this.KT_btn1.UseVisualStyleBackColor = false;
+            this.KT_btn1.Click += new System.EventHandler(this.KT_btnArvuta_Click);
+            // 
+            // KT_lbl3
+            // 
+            this.KT_lbl3.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.KT_lbl3.Location = new System.Drawing.Point(98, 331);
+            this.KT_lbl3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.KT_lbl3.Name = "KT_lbl3";
+            this.KT_lbl3.Size = new System.Drawing.Size(122, 32);
+            this.KT_lbl3.TabIndex = 9;
+            this.KT_lbl3.Text = "Vastus";
+            this.KT_lbl3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // KT_txtVastus
+            // 
+            this.KT_txtVastus.ForeColor = System.Drawing.Color.DarkBlue;
+            this.KT_txtVastus.Location = new System.Drawing.Point(241, 335);
+            this.KT_txtVastus.Name = "KT_txtVastus";
+            this.KT_txtVastus.Size = new System.Drawing.Size(273, 26);
+            this.KT_txtVastus.TabIndex = 10;
+            this.KT_txtVastus.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // KT_btn2
+            // 
+            this.KT_btn2.BackColor = System.Drawing.Color.Lavender;
+            this.KT_btn2.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.KT_btn2.Location = new System.Drawing.Point(333, 422);
+            this.KT_btn2.Name = "KT_btn2";
+            this.KT_btn2.Size = new System.Drawing.Size(150, 39);
+            this.KT_btn2.TabIndex = 11;
+            this.KT_btn2.Text = "Tühjenda väljad";
+            this.KT_btn2.UseVisualStyleBackColor = false;
+            this.KT_btn2.Click += new System.EventHandler(this.KT_btnTyhjenda_Click);
+            // 
+            // KT_btnYmarda
+            // 
+            this.KT_btnYmarda.BackColor = System.Drawing.Color.Lavender;
+            this.KT_btnYmarda.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.KT_btnYmarda.Location = new System.Drawing.Point(564, 323);
+            this.KT_btnYmarda.Name = "KT_btnYmarda";
+            this.KT_btnYmarda.Size = new System.Drawing.Size(128, 48);
+            this.KT_btnYmarda.TabIndex = 12;
+            this.KT_btnYmarda.Text = "Ümarda täisarvuks";
+            this.KT_btnYmarda.UseVisualStyleBackColor = false;
+            this.KT_btnYmarda.Click += new System.EventHandler(this.KT_btnYmarda_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(772, 623);
+            this.Controls.Add(this.KT_btnYmarda);
+            this.Controls.Add(this.KT_btn2);
+            this.Controls.Add(this.KT_txtVastus);
+            this.Controls.Add(this.KT_lbl3);
+            this.Controls.Add(this.KT_btn1);
+            this.Controls.Add(this.KT_rbJaga);
+            this.Controls.Add(this.KT_rbKorruta);
+            this.Controls.Add(this.KT_rbLahuta);
+            this.Controls.Add(this.KT_rbLiida);
+            this.Controls.Add(this.KT_txtArv2);
+            this.Controls.Add(this.KT_txtArv1);
+            this.Controls.Add(this.KT_lbl2);
+            this.Controls.Add(this.KT_lbl1);
+            this.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Form1";
+            this.Text = "Kalkulaator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label KT_lbl1;
+        private System.Windows.Forms.Label KT_lbl2;
+        private System.Windows.Forms.TextBox KT_txtArv1;
+        private System.Windows.Forms.TextBox KT_txtArv2;
+        private System.Windows.Forms.RadioButton KT_rbLiida;
+        private System.Windows.Forms.RadioButton KT_rbLahuta;
+        private System.Windows.Forms.RadioButton KT_rbKorruta;
+        private System.Windows.Forms.RadioButton KT_rbJaga;
+        private System.Windows.Forms.Button KT_btn1;
+        private System.Windows.Forms.Label KT_lbl3;
+        private System.Windows.Forms.TextBox KT_txtVastus;
+        private System.Windows.Forms.Button KT_btn2;
+        private System.Windows.Forms.Button KT_btnYmarda;
+    }
+}
+
---
+++ Kalkulaator/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kalkulaator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void KT_btnArvuta_Click(object sender, EventArgs e)
+        {            
+            double esimeneArv; 
+            double teineArv; 
+
+            if (double.TryParse(KT_txtArv1.Text, out esimeneArv))
+            {
+                if (double.TryParse(KT_txtArv2.Text, out teineArv))
+                {
+                    if (KT_rbLiida.Checked)
+                        KT_txtVastus.Text = (esimeneArv + teineArv).ToString();
+                    else if (KT_rbLahuta.Checked)
+                        KT_txtVastus.Text = (esimeneArv - teineArv).ToString();
+                    else if (KT_rbKorruta.Checked)
+                        KT_txtVastus.Text = (esimeneArv * teineArv).ToString();
+                    else if (KT_rbJaga.Checked)
+                        if (teineArv == 0)
+                        {
+                            // jagamistehte korral tuleb teade selle kohta, et teine arv on 0
+                            MessageBox.Show("Nulliga ei saa jagada!");
+                            KT_txtArv2.Text = "";
+                            KT_txtArv2.Focus();
+                        }
+                        else
+                            KT_txtVastus.Text = (esimeneArv / teineArv).ToString();
+                    else
+                        MessageBox.Show("Vali tehe!");  // teade, et tehet ei ole valitud                    
+                }
+                // juhul, kui Parse ei õnnestu (arvu asemel on sisestatud täht, 
+                // komaarvus on koma asemel punkt või arv on sisestamata), tuleb selle kohta teade
+                else
+                {
+                    KT_txtArv2.Text = "";
+                    KT_txtArv2.Focus();
+                    MessageBox.Show("Sisesta korrektne arv (ära kasuta tähti, komakohad eralda koma, mitte punktiga)!");
+                }
+            }
+            else
+            {
+                KT_txtArv1.Text = "";
+                KT_txtArv1.Focus();
+                MessageBox.Show("Sisesta korrektne arv (ära kasuta tähti, komakohad eralda koma, mitte punktiga)!");
+            }
+            }
+
+            // Nupp, mis tühjendab kõik väljad ja valikud, viib kursori esimese arvu sisestamise kasti
+            private void KT_btnTyhjenda_Click(object sender, EventArgs e)
+            {
+                KT_rbLiida.Checked = false;
+                KT_rbLahuta.Checked = false;
+                KT_rbKorruta.Checked = false;
+                KT_rbJaga.Checked = false;
+                KT_txtVastus.Text = "";
+                KT_txtArv2.Text = "";
+                KT_txtArv1.Text = "";
+                KT_txtArv1.Focus();
+            }
+
+            // Nupp, millega saab ümardada komakohaga vastuse täisarvuks
+            private void KT_btnYmarda_Click(object sender, EventArgs e)
+            {
+                double vastus = double.Parse(KT_txtVastus.Text);
+                double ymarVastus = Math.Round(vastus);
+                KT_txtVastus.Text = ymarVastus.ToString();
+            }
+        }
+    }
